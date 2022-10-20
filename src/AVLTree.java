b94e3df9b7d7c680fc12eed0df69b702df963ae5
邓天骄 <jiao_snow@126.com>
2022-10-20 19:09:33 +0800

    忽略文件

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -16,19 +16,101 @@ public class AVLTree<T extends Comparable<T>> {
         return a > b ? a : b;
     }
 
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * LL: 左左对应的情况(左单旋转)。
+     *
+     * @param k2
+     * @return 旋转后的根节点
+     */
+    private AVLTreeNode<T> leftLeftRotating(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1;
+        k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+
+        k1.height = this.max(this.height(k1.left), this.height(k2.right)) + 1;
+        k2.height = this.max(this.height(k2.left), this.height(k2.right) + 1);
+        return k1;
+    }
+
+    /**
+     * RR: 右右对应的情况（右单旋转）
+     *
+     * @param k2 k2
+     * @return 旋转后的根节点
+     */
+    private AVLTreeNode<T> rightRightRotating(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1;
+        k1 = k2.right;
+        k2.right = k1.left;
+        k1.left = k2;
+
+        k1.height = this.max(height(k1.left), height(k1.right)) + 1;
+        k2.height = this.max(height(k2.left), height(k2.right)) + 1;
+        return k1;
+    }
+
+
+    /**
+     * LR: 左右对应的情况(左双旋转)。
+     *
+     * @param k2 k2
+     * @return {@link AVLTreeNode}<{@link T}> 旋转后的根节点
+     */
+    private AVLTreeNode<T> LeftRigthRotating(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1 = this.leftLeftRotating(k2.left);
+        return this.rightRightRotating(k1);
+    }
+
+    /**
+     * 右左对应的情况(右双旋转)。
+     *
+     * @param k2 k2
+     * @return {@link AVLTreeNode}<{@link T}> 旋转后的根节点
+     */
+    private AVLTreeNode<T> rigthLeftRotating(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1 = this.rightRightRotating(k2.right);
+        return this.leftLeftRotating(k1);
+    }
+
+    /*
+     * 将结点插入到AVL树中，并返回根节点
+     *
+     * 参数说明:
+     *     tree AVL树的根结点
+     *     key 插入的结点的键值
+     * 返回值:
+     *     根节点
+     */
+    private AVLTreeNode<T> insert(AVLTreeNode<T> tree, T key) {
+        if (tree == null) {
+            tree = new AVLTreeNode<>(key, null, null);
+        } else {
+//            key.compareTo()
+        }
+
+        tree.height = tree.height + 1;
+        return tree;
+    }
+
+    public void insert(T key) {
+        mRoot = insert(mRoot, key);
+    }
+
     private class AVLTreeNode<T> {
         private T key;
         private AVLTreeNode<T> left;
         private AVLTreeNode<T> right;
         private int height;
 
-        public AVLTreeNode(T key, AVLTreeNode<T> left, AVLTreeNode<T> right, int height) {
+        public AVLTreeNode(T key, AVLTreeNode<T> left, AVLTreeNode<T> right) {
             this.key = key;
             this.left = left;
             this.right = right;
-            this.height = height;
         }
     }
-
-
 }
